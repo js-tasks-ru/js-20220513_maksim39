4c50fbb9fecb0027908f9968af3df11a1933710b
Турнаев Максим Анатольевич <m.turnaev@cft.ru>
2022-05-23 13:22:25 +0700

    block 2. implemented sortStrings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,18 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const arrCopy = [...arr];
+  if (param === 'asc') {
+    return arrCopy.sort((a, b) => compare(a, b));
+  } else {
+    return arrCopy.sort((a, b) => compare(b, a));
+  }
 
+  function compare(a, b) {
+    const comparison = a.toLowerCase().localeCompare(b.toLowerCase());
+    if (comparison === 0) {
+      return a > b ? 1 : (a === b) ? 0 : -1;
+    }
+    return comparison;
+  }
 }
