02ac07c215bfe3b1395bed0d48bece43e723d485
Турнаев Максим Анатольевич <m.turnaev@cft.ru>
2022-05-24 21:15:51 +0700

    task 3.2 implemented revertObj

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,10 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  if (obj === undefined) {
+    return undefined;
+  }
+  return Object.fromEntries(Object.entries(obj).map(function (current) {
+    return [current[1], current[0]];
+  }));
 }
